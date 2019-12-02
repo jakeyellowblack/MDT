8ae55b3efa321188241e67312803d5ac3899a78a
jakeyellowblack <51396898+jakeyellowblack@users.noreply.github.com>
2019-12-01 23:54:13 -0400

    Se instalo Laravel Collective

---
+++ MDT/database/seeds/CategorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use MDT\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
