6840a656d517e51be595ab6b1dc6e874c0a5257a
jakeyellowblack <51396898+jakeyellowblack@users.noreply.github.com>
2019-12-03 01:49:57 -0400

    Corregido CategoryTableSeeder

--- MDT/database/seeds/DatabaseSeeder.php
+++ MDT/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
 		$this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        //$this->call(CategoryTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
     }
 }
