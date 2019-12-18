837635389c733b11ae5b9206836a9d6188fa35c3
Jonmlr <Jonmlr@users.noreply.github.com>
2019-12-18 04:40:20 -0400

    Ahorro de código

---
+++ MDT/resources/views/Layout/upbar.blade.php
@@ -0,0 +1,313 @@
+<div class="wrapper">	
+		<header>
+			<div class="container">
+				<div class="header-data">
+					<div class="logo">
+						<a href="home" title=""><img src="images/logo-mdt.png" alt=""></a>
+					</div><!--logo end-->
+                    
+<!-------------------------------------------INICIO DE BARRA DE BÚSQUEDA---------------------------------------->                  
+					<div class="search-bar">
+						<form>
+							<input type="text" name="search" placeholder="Search...">
+							<button type="submit"><i class="la la-search"></i></button>
+						</form>
+					</div><!--search-bar end-->
+                    
+<!-------------------------------------------FIN DE BARRA DE BÚSQUEDA------------------------------------------->
+                    
+                    
+<!-------------------------------------------INICIO DE BARRA DE NAVEGACIÓN-------------------------------------->
+
+					<nav>
+						<ul>
+                        
+                        <!--Icono de Home-->
+                        
+							<li>
+								<a href="home" title="">
+									<span><img src="images/icon1.png" alt=""></span>
+									Home
+								</a>
+							</li>
+                            
+                        <!--Fin Icono Home-->
+                            
+                            
+                        <!--Icono de Companies (Oculto BTM)--> 
+                        
+							<!--<li>
+								<a href="companies.html" title="">
+									<span><img src="images/icon2.png" alt=""></span>
+									Companies
+								</a>
+								<ul>
+									<li><a href="companies.html" title="">Companies</a></li>
+									<li><a href="company-profile.html" title="">Company Profile</a></li>
+								</ul>
+							</li>-->
+                            
+                         <!--Fin Icono Companies (Oculto BTM)-->
+                            
+                            
+                         <!--Icono de Projects--> 
+                           
+							<li>
+								<a href="projects.html" title="">
+									<span><img src="images/icon3.png" alt=""></span>
+									Projects
+								</a>
+							</li>
+                            
+                         <!--Fin Icono Projects-->
+                            
+                            
+                         <!--Icono de Profiles-->  
+                          
+							<li>
+								<a href="profiles.html" title="">
+									<span><img src="images/icon4.png" alt=""></span>
+									Profiles
+								</a>
+								<ul>
+									<li><a href="user-profile.html" title="">User Profile</a></li>
+									<li><a href="my-profile-feed.html" title="">my-profile-feed</a></li>
+								</ul>
+							</li>
+                            
+                         <!--Fin Icono Profiles-->
+                         
+                         
+                         <!--Icono de Jobs-->
+                         
+							<li>
+								<a href="jobs.html" title="">
+									<span><img src="images/icon5.png" alt=""></span>
+									Jobs
+								</a>
+							</li>
+                            
+                         <!--Fin Icono Jobs-->
+                         
+                         
+                         <!--Icono de Messages-->
+                         
+							<li>
+								<a href="#" title="" class="not-box-openm">
+									<span><img src="images/icon6.png" alt=""></span>
+									Messages
+								</a>
+								<div class="notification-box msg" id="message">
+									<div class="nt-title">
+										<h4>Setting</h4>
+										<a href="#" title="">Clear all</a>
+									</div>
+									<div class="nott-list">
+										<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img1.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="messages.html" title="">Jassica William</a> </h3>
+							  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.</p>
+							  					<span>2 min ago</span>
+							  				</div><!--notification-info -->
+						  				</div>
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img2.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="messages.html" title="">Jassica William</a></h3>
+							  					<p>Lorem ipsum dolor sit amet.</p>
+							  					<span>2 min ago</span>
+							  				</div><!--notification-info -->
+						  				</div>
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img3.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="messages.html" title="">Jassica William</a></h3>
+							  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempo incididunt ut labore et dolore magna aliqua.</p>
+							  					<span>2 min ago</span>
+							  				</div><!--notification-info -->
+						  				</div>
+						  				<div class="view-all-nots">
+						  					<a href="messages.html" title="">View All Messsages</a>
+						  				</div>
+									</div><!--nott-list end-->
+								</div><!--notification-box end-->
+							</li>
+                            
+                      <!--Fin Icono Messages-->
+                            
+                            
+                           <!--Icono de Notificacions-->
+                           
+							<li>
+								<a href="#" title="" class="not-box-open">
+									<span><img src="images/icon7.png" alt=""></span>
+									Notification
+								</a>
+                                
+								<div class="notification-box noti" id="notification">
+									<div class="nt-title">
+										<h4>Setting</h4>
+										<a href="#" title="">Clear all</a>
+									</div>
+                                    
+									<div class="nott-list">
+										<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img1.png" alt="">
+							  				</div>
+                                            
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div><!--notification-info -->
+						  				</div>
+                                        
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img2.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div><!--notification-info -->
+						  				</div>
+                                        
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img3.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div><!--notification-info -->
+						  				</div>
+                                        
+						  				<div class="notfication-details">
+							  				<div class="noty-user-img">
+							  					<img src="images/resources/ny-img2.png" alt="">
+							  				</div>
+							  				<div class="notification-info">
+							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
+							  					<span>2 min ago</span>
+							  				</div><!--notification-info -->
+						  				</div>
+                                        
+						  				<div class="view-all-nots">
+						  					<a href="#" title="">View All Notification</a>
+						  				</div>
+									</div><!--nott-list end-->
+								</div><!--notification-box end-->
+							</li>
+                        </ul>
+					</nav><!--nav end-->
+                            
+                     <!--Fin Icono Notificacions-->
+                         
+					
+             <!--Botón de Menú-->
+             
+					<div class="menu-btn">
+						<a href="#" title=""><i class="fa fa-bars"></i></a>
+					</div><!--menu-btn end-->
+                    
+             <!--Fin de Botón de Menú-->
+                    
+                    
+             <!--Muestra del nombre de la persona Logueada--> 
+             
+					<div class="user-account">
+						<div class="user-info">
+							<img src="images/resources/user.png" alt="">
+                            <a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->firstname }} <span class="caret"></span>
+                                    
+                                </a>
+							<i class="la la-sort-down"></i>
+						</div>
+                        
+             <!--Fin Muestra de nombre-->
+                        
+                        
+             <!--Sección de "Online Status"--> 
+             
+						<div class="user-account-settingss" id="users">
+							<h3>Online Status</h3>
+							<ul class="on-off-status">
+								<li>
+									<div class="fgt-sec">
+										<input type="radio" name="cc" id="c5">
+										<label for="c5">
+											<span></span>
+										</label>
+										<small>Online</small>
+									</div>
+								</li>
+								<li>
+									<div class="fgt-sec">
+										<input type="radio" name="cc" id="c6">
+										<label for="c6">
+											<span></span>
+										</label>
+										<small>Offline</small>
+									</div>
+								</li>
+							</ul>
+                            
+             <!--Fin de Sección "Online Status"-->
+                            
+                            
+                    <!--Sección de "Custom Status"--> 
+                           
+							<h3>Custom Status</h3>
+							<div class="search_form">
+								<form>
+									<input type="text" name="search">
+									<button type="submit">Ok</button>
+								</form>
+							</div><!--search_form end-->
+                            
+                     <!--Fin de Sección "Online Status"-->
+                     
+                     
+                     <!--Sección de "Settings"-->
+                     
+							<h3>Setting</h3>
+							<ul class="us-links">
+								<li><a href="profile-account-setting.html" title="">Account Setting</a></li>
+								<li><a href="#" title="">Privacy</a></li>
+								<li><a href="#" title="">Faqs</a></li>
+								<li><a href="#" title="">Terms & Conditions</a></li>
+							</ul>
+                            
+                     <!--Fin de Sección "Settings"-->
+                            
+                            
+                     <!--Sección de "Logout"-->
+                            
+                            <h3 class="tc"><a class="dropdown-item" href="{{ route('register') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a></h3>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                    
+                      <!--Fin de Sección "Logout"-->
+                           
+						</div><!--user-account-settingss end-->
+					</div>
+				</div><!--header-data end-->
+			</div>
+		</header><!--header end-->	
+        
+<!--------------------------------------------FIN DE BARRA DE NAVEGACIÓN------------------------------------------------>
+
